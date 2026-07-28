760e5bf082c335df0196f57dad9849edb59737bf
Emmanuel Oludare <emmanueldavidson524@gmail.com>
2026-07-28 14:50:58 +0100

    Updates to backend

--- inventory-management-server/src/index.ts
+++ inventory-management-server/src/index.ts
@@ -32,7 +32,7 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/expenses", expensesRoute);
 
 // SERVER
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
